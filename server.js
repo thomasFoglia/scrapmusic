675d16bb7addd25c5cf5fb20e7cb5ffc43e65679
risq <valentin.ledrapier@gmail.com>
2014-05-23 10:50:46 +0200

    update

--- server.js
+++ server.js
@@ -1,13 +1,73 @@
 var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var async = require('async');
-var app = express();
+    fs      = require('fs'),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    async   = require('async'),
+    path    = require('path'),
+    app     = express();
+
+var html_dir = './views/';
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.sendfile(html_dir + 'index.html');
+});
+
+app.get('/scrape', function (req, res) {
+    async.parallel([
+        function(callback){
+            search_mp3clan(req.query.q, callback);
+        }
+    ], function (error, response) {
+        res.json(response);
+    });
+});
+
+app.get('/dl', function (req, res) {
+    download('http://mp3clan.com/url.php?tid=1331848_78099493&name=2pac%20-%20Lil%27%20Homies',
+        'test.mp3', function() {
+            console.log('done');
+        });
+});
+
+app.listen('8081');
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
 
 
+function search_mp3clan(search, callback) {
+    url = 'http://mp3clan.com/mp3/' + search + '.html';
 
-var download = function(uri, filename, callback){
+    request(url, function (error, response, html) {
+        if (!error) {
+            scrapeResultsPage_mp3clan(html, function(results) {
+                callback(null, results);
+            });
+        }
+        else {
+            callback(null, 'error');
+        }
+    });    
+}
+
+function scrapeResultsPage_mp3clan(html, callback) {
+    var $ = cheerio.load(html);
+    var title, bitrate, size;
+    var results = [];
+
+    $('#mp3list .mp3list-table').each(function (i, el) {
+        results.push({
+            title:  $(this).find('.unselectable').text().trim(),
+            link:   $(this).find('.mp3list-download a').attr('href')
+        });
+    });
+
+    callback(results);
+}
+
+
+function download(uri, filename, callback) {
     var jar = request.jar();
     var cookie = request.cookie("download=approved");
     jar.setCookie(cookie, uri);
@@ -22,78 +82,6 @@ var download = function(uri, filename, callback){
         }).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
 };
-
-download('http://mp3clan.com/url.php?tid=1331848_78099493&name=2pac%20-%20Lil%27%20Homies',
-    'test.mp3', function(){
-  console.log('done');
-});
-
-
-app.get('/scrape', function (req, res) {
-
-
-
-    url = 'http://mrtzcmp3.net/search/kygo_sexual_healing';
-
-    var search = "claude_francois";
-
-    async.parallel([
-        search_mp3clan(search)
-    ], function (error,response) {
-        console.log(response);
-    });
-
-
-    request(url, function (error, response, html) {
-        if (!error) {
-            scrapeResultsPage(html, function(results) {
-                res.send(results);
-            });
-        }
-    })
-})
-
-
-
-
-
-app.listen('8081');
-console.log('Magic happens on port 8081');
-exports = module.exports = app;
-
-
-
-
-function search_mp3list(search) {
-    url = 'http://mp3clan.com/mp3/' + search + '.html';
-
-    request(url, function (error, response, html) {
-        if (!error) {
-            scrapeResultsPage_mp3clan(html, function(results) {
-                res.send(results);
-            });
-        }
-    });
-}
-
-
-
-function scrapeResultsPage_mp3clan(html, callback) {
-    var $ = cheerio.load(html);
-
-    var title, bitrate, size;
-    var links = [];
-
-    $('.mp3list').each(function (i, el) {
-        links.push({
-                id_api:  $(this).find('script').text().slice(18, -2).trim(),
-                id_file: $(this).find('span.mp3Title').attr('id')
-            });
-    });
-}
-
-
-
 
 // function scrapeResultsPage(html, callback) {
 //     var $ = cheerio.load(html);
